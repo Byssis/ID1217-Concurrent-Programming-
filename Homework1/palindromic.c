b7b67a4ac145b229fed2ce8d5b82c8cfd56240ca
Albin Bystrom <albinby@kth.se>
2018-01-21 18:05:40 +0100

    Fixing input to palindromic

--- Homework1/palindromic.c
+++ Homework1/palindromic.c
@@ -5,7 +5,7 @@
 #include <time.h>
 #include <string.h>
 #include <sys/time.h>
-
+#include "bench.h"
 #define MAXWORKERS 10
 #define WORDLENGTH 40
 #define MAXSIZE 25143
@@ -67,20 +67,6 @@ void * Worker(void * args){
     if(result != -1)
       printf("%s %s\n", word, flip);
   }
-}
-
-/* timer */
-double read_timer() {
-    static bool initialized = false;
-    static struct timeval start;
-    struct timeval end;
-    if( !initialized )
-    {
-        gettimeofday( &start, NULL );
-        initialized = true;
-    }
-    gettimeofday( &end, NULL );
-    return (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
 }
 
 int main(int argc, char *argv[]){
