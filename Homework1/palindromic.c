4b9847a1bd2233465fd462e9a3058e7833cb191a
Albin Bystrom <albinby@kth.se>
2018-01-29 16:55:55 +0100

    palindromic

--- Homework1/palindromic.c
+++ Homework1/palindromic.c
@@ -98,7 +98,7 @@ int main(int argc, char *argv[]){
   while(fscanf(file,"%s",dictionary[k++]) == 1);
   fclose(file);
   size = k + 1;
-
+  printf("Size: %d\n", size);
   int numWorkers =  MAXWORKERS;
   pthread_t workerid[numWorkers];
 
