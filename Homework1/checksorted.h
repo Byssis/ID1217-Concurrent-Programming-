1ac32dafee5bb3c05e6484933c5b303fbd40b17a
Albin Bystrom <albinby@kth.se>
2018-01-29 16:26:27 +0100

    Add check to quicksort

--- Homework1/checksorted.h
+++ Homework1/checksorted.h
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-int ifsorted(int * arrry, int size){
+int ifsorted(int * array, int size){
     int prev = *(array++);
     int i = 0;
     for (i = 1; i < size; i++) {
