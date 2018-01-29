9ec082b268a250259d56cb123952f5e66263e463
Albin Bystrom <albinby@kth.se>
2018-01-29 21:24:53 +0100

    Add pi program

--- Homework1/pi.c
+++ Homework1/pi.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include "bench.h"
 
-#define ERROR 0.00000001
+pthread_mutex_t lock;
+
+double error;
+int num_workers;
 
 double fun(double x){
   double result;
@@ -13,8 +17,8 @@ double fun(double x){
 }
 
 double dabs(double x){
-  if(x > 0.0) return x;
-  else return x * -1.0;
+  if(x > 0.0)   return x;
+  else          return x * -1.0;
 }
 
 int i = 0;
@@ -26,21 +30,25 @@ double quad(double l, double r, double fl, double fr, double area){
   larea = ( fl + fm ) * ( m - l ) / 2;
   rarea = ( fl + fr ) * ( r - m ) / 2;
   a = dabs((larea + rarea) - area);
-  if(dabs((larea + rarea) - area) > ERROR){
+  if(dabs((larea + rarea) - area) > error){
     larea = quad(l, m, fl, fm, larea);
     rarea = quad(m, r, fm, fr, rarea);
   }
   return (larea + rarea);
 }
 
-int main() {
-  double a, b, area, fa, fb,result;
+int main(int argc, char *argv[]) {
+  double a, b, area, fa, fb,result, start_time, end_time;
+  error = atoi(argv[1]);
+  start_time = read_timer();
   a = 0;
   b = 1;
   fa = fun(a);
   fb = fun(b);
   area = ( fa + fb ) * ( fb - fa ) / 2;
   result = 4 * quad(a, b, fa, fb, area);
+  end_time = read_timer();
   printf("%f\n", result);
+  printf("The execution time is %g sec\n", end_time - start_time);
   return 0;
 }
